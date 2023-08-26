a4c1a31f214c7894a235006926259b5fa3dcb238
mononokai <leachda12@icloud.com>
2023-08-26 19:27:30 -0400

    actually completed basics 03

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -3,8 +3,46 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: '',
+      lastName: '',
       confirmedName: '',
+      // fullname: ''
     };
+  },
+  watch:{
+    counter(value) {
+      if (value > 50) {
+        // storing this in that in order to bring the object up to the timeout function
+        const that = this;
+        setTimeout(() => {
+          that.counter = 0;
+        }, 2000)
+      }
+    }
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   }
+    //   else {
+    //     this.fullname = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName() {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   }
+    //   else {
+    //     this.fullname = this.name + ' ' + value;
+    //   }
+    // }
+  },
+  computed: {
+    fullname() {
+      if (this.name === '' || this.lastName === '') {
+        return '';
+      }
+
+      return this.name + ' ' + this.lastName;
+    }
   },
   methods: {
     add(num) {
@@ -14,13 +52,23 @@ const app = Vue.createApp({
       this.counter = this.counter - num;
     },
     setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+      this.name = event.target.value;
     },
     submitForm(event) {
       alert('Submitted!');
     },
     confirmInput() {
       this.confirmedName = this.name;
+    },
+    resetInput() {
+      this.name = '';
+    },
+    outputFullname() {
+      if (this.name === '') {
+        return '';
+      }
+
+      return this.name + ' ' + "Leach";
     }
   }
 });
